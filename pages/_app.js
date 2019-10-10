82618ecd9e076dd0b38bdef38131428c561f4470
Robin Millette <robin@millette.info>
2019-10-10 09:58:34 -0400

    Extend nextjs app.

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+// npm
+import App from "next/app"
+import { MDXProvider } from "@mdx-js/react"
+
+import Comp from "../docs/comp-1.mdx"
+
+const components = {
+  Comp: () => 'Foo'
+}
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+          <MDXProvider components={components}>
+              <Component {...pageProps} />
+          </MDXProvider>
+    )
+  }
+}
+
+export default MyApp
